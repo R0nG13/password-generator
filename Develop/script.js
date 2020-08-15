abdea623a40cf499c6b211508468ecade4d9f81a
Alyssa Klopfenstein <alyssa.r.purdy@gmail.com>
2020-08-15 18:56:10 -0500

    add screenshot for README.md

--- Develop/script.js
+++ Develop/script.js
@@ -47,15 +47,34 @@ function generatePassword() {
     }
   }
 
-  // create variable to hold generated password
-  var genPassword = "";
+  var genPassword;
+  var validPassword = false;
 
-  // create a for loop which runs "passwordLength"
-  for (var i = 0; i < passwordLength; i++) {
-    // each iteration, select a random character from "chars"
-    var index = Math.floor(Math.random() * (chars.length - 1));
-    // add selected character onto password variable
-    genPassword += chars[index];
+  while (!validPassword) {
+    // create variable called "typesUsed" to store charTypes used (array)
+
+    // create variable to hold generated password
+    genPassword = "";
+
+    // create a for loop which runs "passwordLength"
+    for (var i = 0; i < passwordLength; i++) {
+      // each iteration, select a random character from "chars"
+      var index = Math.floor(Math.random() * (chars.length - 1));
+
+      // create variable for store selected char (refactor "genPassword += ..." to use this variable)
+        var storeSelectedChar;
+        genPassword += storeSelectedChar[index];
+      // create a for loop to loop through selectedTypes
+       // for each time through the loop, check to see if the selected character is in that specific type's "charTypes" value (hint: charTypes[selectedType].indexOf(char) >= 0)
+      // if it IS, push the charType key into the array you created at the top of this main while loop
+
+      // add selected character onto password variable
+      genPassword += chars[index];
+    }
+
+    // once password is done being gnerated, set "validPassword" to true
+    // create ONE MORE for loop to go through "selectedTypes", make sure they each of them is in "typesUsed" at least once
+    // for each time through this loop, if a selected type is NOT found in "typesUsed", set "validPassword" to false and break out of this for loop immediately
   }
 
   // return password variable
