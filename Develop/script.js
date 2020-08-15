882addae030dd9b0034a1f8a5712316ae583da83
Alyssa Klopfenstein <alyssa.r.purdy@gmail.com>
2020-08-15 11:51:53 -0500

    create generatePassword function and set password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,22 @@
 // Assignment code here
 
+function generatePassword() {
+  // prompt for password length between 8-128 characters
+  var passwordLength;
+
+  while (!passwordLength || passwordLength < 8 || passwordLength > 128) { 
+    passwordLength = window.prompt("Please choose your password length between 8 and 128 characters");
+    passwordLength = parseInt(passwordLength);
+  }
+
+  // prompt for character types including lowercase, uppercase, numeric, and/or special characters
+  // validate input and at least one character type should be selected
+  // make array to store selected char types, create while loop and while that array is empty keep asking for char types
+  // probably once a user has selected a char type an empty value should terminate the loop
+
+  // password generates in alert window or written to the page
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
