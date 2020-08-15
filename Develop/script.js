ee632d8ff479976a7823aa7eda52ac73ca6bde28
Alyssa Klopfenstein <alyssa.r.purdy@gmail.com>
2020-08-15 12:56:39 -0500

    add character type selection

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,17 @@
 // Assignment code here
 
 function generatePassword() {
+  var chars = {
+    lowercase: "abcdefghijklmnopqrstuvwxyz",
+    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numeric: "0123456789",
+    special: "!@#$%&*()-_.?+="
+  };
+
   // prompt for password length between 8-128 characters
   var passwordLength;
 
-  while (!passwordLength || passwordLength < 8 || passwordLength > 128) { 
+  while (!passwordLength || passwordLength < 8 || passwordLength > 128) {
     passwordLength = window.prompt("Please choose your password length between 8 and 128 characters");
     passwordLength = parseInt(passwordLength);
   }
@@ -13,8 +20,29 @@ function generatePassword() {
   // validate input and at least one character type should be selected
   // make array to store selected char types, create while loop and while that array is empty keep asking for char types
   // probably once a user has selected a char type an empty value should terminate the loop
+  var charTypes = [];
+  var charType;
 
-  // password generates in alert window or written to the page
+  while (charTypes.length === 0 || charType) {
+    charType = window.prompt("Please choose a character type. Select cancel when done. Valid types: " + Object.keys(chars).join(', '));
+
+    if (charType === null) {
+      continue;
+    }
+
+    charType = charType.toLowerCase();
+
+    if (charType in chars) {
+    // if (charType === "lowercase" || charType === "uppercase" || charType === "numeric" || charType === "special") {
+      if (charTypes.indexOf(charType) === -1) {
+        charTypes.push(charType);
+      } else {
+        window.alert("You've already selected " + charType + ".");
+      }
+    }
+  }
+
+  console.log(charTypes);
 }
 
 
